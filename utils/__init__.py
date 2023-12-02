61959cafce6b1fe25a2f6bbdaaacd32c36fe5326
Semen Borinskikh <ph.borinskikh@mail.ru>
2023-12-03 00:56:37 +0300

    add ML files

---
+++ utils/__init__.py
@@ -0,0 +1,5 @@
+from .data_processing import split_to_x_y, numeric_transform, balance_the_dataset, \
+    best_hyperparams_transform
+from .graphics import plot_feature_importance, plot_metrics_hist
+from .ml_scripts import hp_optimize, model_fit_predict, print_classif_report
+
---
+++ utils/get_predict.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import joblib
+from sklearn.metrics import recall_score, f1_score, precision_score
+
+from utils.feature_selection import feature_selection
+from .data_processing import split_to_x_y
+
+
+def get_predict(test_data):
+    # Загрузка модели из файла
+    filename = 'model.pkl'
+    loaded_model = joblib.load(filename)
+
+
+    # Подготовка к получению предтика
+    X, y = split_to_x_y(test_data, 'y')
+
+    predictions = loaded_model.predict(X.values)
+    predictions_proba = loaded_model.predict_proba(X.values)
+
+    result = pd.DataFrame(predictions_proba)
+    result.index = test_data.index
+    result.to_csv('result_example.csv', index=False)
+
+    # Метрики
+    recall = recall_score(y_true=y, y_pred=predictions, average='weighted')
+    f1 = f1_score(y_true=y, y_pred=predictions, average='weighted')
+    precision = precision_score(y_true=y, y_pred=predictions, average='weighted')
+    return recall, f1, precision
