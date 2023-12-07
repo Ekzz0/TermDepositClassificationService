38c631b4b5c49e6fee58bfa706e36fc4a0b2544c
Алексей <zaykuh@gmail.com>
2023-12-07 16:51:39 +0500

    feat: add get post in js

--- APP/src/main/resources/static/js/diagram.js
+++ APP/src/main/resources/static/js/diagram.js
@@ -1,25 +1,26 @@
-const ctx = document.getElementById('myChart');
-const dates = ["15.1.2000", "16.1.2000", "17.1.2000", "20.1.2000"]
-const probability = [10, 20, 40, 50];
+function diagram(dates, probability){
+  const ctx = document.getElementById('myChart');
+  const dates = dates;
+  const probability = probability;
 
-chart =  new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: dates,
-      datasets: [{
-        label: '% вероятность ',
-        data: probability,
-        borderColor: '#36A2EB',
-        backgroundColor: '#9BD0F5',
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: false
+  chart =  new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: dates,
+        datasets: [{
+          label: '% вероятность увольнения',
+          data: probability,
+          borderColor: '#36A2EB',
+          backgroundColor: '#9BD0F5',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: false
+          }
         }
       }
-    }
-});
-console.log(chart)
+  });
+}
